635ce93c2c6d0f3fcab29034383c949678ac50d1
Samuel Etling <samueletling@gmail.com>
2024-08-04 20:07:09 -0600

    added the files oops

---
+++ index.js
@@ -0,0 +1,112 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+// Array of questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a description of your project:",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "How do you install your project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How do you use your project?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license for your project:",
+    choices: ["MIT", "GPLv3", "Apache 2.0", "BSD 3-Clause", "None"],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "How can others contribute to your project?",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "How do you test your project?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+];
+
+// Function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log("README.md created successfully!")
+  );
+}
+
+// Function to generate markdown for README
+function generateMarkdown(data) {
+  return `
+# ${data.title}
+
+![License](https://img.shields.io/badge/license-${data.license}-green)
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For any questions, please contact me with the information below:
+
+GitHub: [${data.github}](https://github.com/${data.github})
+
+Email: ${data.email}
+`;
+}
+
+// Function to initialize app
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const markdownContent = generateMarkdown(answers);
+    writeToFile("README.md", markdownContent);
+  });
+}
+
+// Function call to initialize app
+init();
